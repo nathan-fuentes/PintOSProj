836bfb38da14e9bae7f323333f9ce6ff9e96c6e2
Sebastian <sarevalo@berkeley.edu>
2023-03-22 07:24:26 +0000

    Started lock acquire

--- src/threads/synch.c
+++ src/threads/synch.c
@@ -173,7 +173,9 @@ void lock_acquire(struct lock* lock) {
   ASSERT(lock != NULL);
   ASSERT(!intr_context());
   ASSERT(!lock_held_by_current_thread(lock));
-  lock_try_acquire()
+  if (lock_try_acquire(&lock)) {
+    
+  }
   sema_down(&lock->semaphore);
   lock->holder = thread_current();
 }
